629b52454e114105d5cbcdfbf735273789c48959
DeeV33 <becerra.alejandro0131@gmail.com>
2022-10-05 16:38:43 -0700

    Update magpie2
    Checking empty isn't working?

--- Unit2/Magpie2/Magpie2.java
+++ Unit2/Magpie2/Magpie2.java
@@ -32,18 +32,46 @@ public class Magpie2 {
 	 * @return a response based on the rules given
 	 */
 	public String getResponse(String statement) {
+		String statementTrim = statement.trim();
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
-			response = "Why so sussy?";
-		} else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0) {
-			response = "Tell me more about your family.";
-		} else {
-			response = getRandomResponse();
+		{
+			if (statement.indexOf("no") >= 0) {
+				response = "Why so negative?";
+			} else if (statement.indexOf("mother") >= 0
+					|| statement.indexOf("father") >= 0
+					|| statement.indexOf("sister") >= 0
+					|| statement.indexOf("brother") >= 0) {
+				response = "Tell me more about your family.";
+			} else if (statement.indexOf("dog") >= 0
+					|| statement.indexOf("cat") >= 0
+					|| statement.indexOf("pet") >= 0) {
+				response = "Tell me more about your pets.";
+			} else if (statement.indexOf("Beeler") >= 0
+					|| statement.indexOf("beeler") >= 0
+					|| statement.indexOf("Mr.beeler") >= 0
+					|| statement.indexOf("mr.beeler") >= 0
+					|| statement.indexOf("mr.Beeler") >= 0
+					|| statement.indexOf("Mr.Beeler") >= 0
+					|| statement.indexOf("Mr. beeler") >= 0
+					|| statement.indexOf("mr. beeler") >= 0
+					|| statement.indexOf("mr. Beeler") >= 0
+					|| statement.indexOf("Mr. Beeler") >= 0) {
+				response = "Mr. Beeler sounds like a very good teacher.";
+			} else if (statement.indexOf("thirst") >= 0
+					|| statement.indexOf("Thirst") >= 0) {
+				response = "You should drink some water.";
+			} else if (statement.indexOf("Raid") >= 0) {
+				response = "Like this program's (not) sponsor? Raid: Shadow Legends?";
+			} else if (statement.indexOf("Honey") >= 0) {
+				response = "Save money with the browser extension, Honey!";
+			} else if (statementTrim == "") {
+				response = "You wrote nothing?";
+			} else {
+				response = getRandomResponse();
+			}
+
+			return response;
 		}
-		return response;
 	}
 
 	/**
@@ -52,7 +80,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -65,6 +93,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Is that so?";
+		} else if (whichResponse == 5) {
+			response = "Interesting...";
 		}
 
 		return response;

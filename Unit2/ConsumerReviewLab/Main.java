715bf1d80d67567f342cae8d5bc26dd5f2c3adda
DeeV33 <becerra.alejandro0131@gmail.com>
2022-11-30 12:44:47 -0800

    ConsumerReviewLab files uploaded

---
+++ Unit2/ConsumerReviewLab/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Review.sentimentVal("suspicious");
+    Review.sentimentVal("great");
+    Review.sentimentVal("outstanding");
+    Review.sentimentVal("horrid");
+    Review.sentimentVal("Truly");
+    Review.sentimentVal("seriously");
+  }
+}

ac380dadf0ff609d1cabe8382c019d03e66dc9fd
DeeV33 <becerra.alejandro0131@gmail.com>
2022-12-06 16:34:11 -0800

    Did stuff
    Added chapter 3 PDF & answer docs. Used test for actual testing

--- test.java
+++ test.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
 public class test {
-    public static void main(String[] args){
-        int iResult, num1 = 25, num2 = 40, num3 = 17, num4 = 5;
-        double fResult, val1 = 17.0, val2 = 12.78;
-        iResult = num1/num4;
-        fResult = (int) (val1 / num4);
-        System.out.print("iResult is " + iResult + "\nfResult is " + fResult);
+    public static void main(String[] args) {
+        /*
+         * String name = "ADRIANA";
+         * Scanner scan = new Scanner(System.in);
+         * int count = 0;
+         * for (int i = 0; i < name.length(); i++) {
+         * if (name.toLowerCase().charAt(i) == 'a') {
+         * count++;
+         * }
+         * }
+         * System.out.println("The letter \'a\' appears in " + name + " " + count +
+         * " times.");
+         */
+        String str = "Sussus Amogus";
+
+        for (int i = str.length() - 1; i <= 0; i--) {
+
+        }
     }
 }
